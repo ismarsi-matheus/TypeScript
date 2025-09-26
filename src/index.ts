0e10b3162f5d5bfe21b7405d51f01eb4039e5de8
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-25 23:57:13 -0300

    Tipos Básicos do TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+// Tipos Básicos
 let age: number = 5;
+const name: string = "Matheus";
+const isValid: boolean = true;
+let idk: any = 5;
+
+idk = "agora sou uma string";
+idk = true;
+
+const ids: number[] = [1, 2, 3, 4, 5];
+const booleans: boolean[] = [true, false, true];
+const names: string[]= ["Matheus", "João", "Maria"];
 
 console.log(age);
