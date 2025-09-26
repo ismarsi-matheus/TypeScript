da0582860d2c90c0281359886b63e182d67b90ca
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-26 00:14:24 -0300

    Tipos basicos

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Tipos Básicos
 let age = 5;
-console.log(age);
+const name = "Matheus";
+const isValid = true;
+let idk = 5;
+idk = "agora sou uma string";
+idk = true;
+const ids = [1, 2, 3, 4, 5];
+const booleans = [true, false, true];
+const names = ["Matheus", "João", "Maria"];
+// Tupla
+const person = ["Matheus", 5, true];
+// Lista de Tuplas
+const people = [
+    ["Matheus", 5],
+    ["João", 10],
+    ["Maria", 15]
+];
+// Intersections
+const produtId = '1';
+//  Enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+})(Direction || (Direction = {}));
+const direction = Direction.Up;
+console.log(direction);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,42 @@ idk = true;
 
 const ids: number[] = [1, 2, 3, 4, 5];
 const booleans: boolean[] = [true, false, true];
-const names: string[]= ["Matheus", "João", "Maria"];
+const names: string[] = ["Matheus", "João", "Maria"];
 
-console.log(age);
+// Tupla
+
+const person: [string, number, boolean] = ["Matheus", 5, true];
+
+// Lista de Tuplas
+
+const people: [string, number][] = [
+  ["Matheus", 5],
+  ["João", 10],
+  ["Maria", 15],
+];
+
+// Intersections
+
+const produtId: string | number | boolean = "1";
+
+//  Enum
+
+enum Direction {
+  Up = 1,
+  Down = 2,
+  Left = "Esquerda",
+  Right = "Direita",
+}
+
+const direction = Direction.Left;
+
+// Type Assertions
+const produtName: any = "boné";
+
+// let itemId = produtName as string;
+
+
+let itemId = <string>produtName;
+
+
+console.log(direction);
