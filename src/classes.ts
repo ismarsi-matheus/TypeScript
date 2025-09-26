6f0cf3b2bc40d6fceb852c01414c724f4d714ccb
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-26 15:57:04 -0300

    Modificadores de Classes (readonly,protected,private)

--- src/classes.ts
+++ src/classes.ts
@@ -1,14 +1,14 @@
 interface IPerson {
   id: number;
-  name: string;
-  age: number;
+//   name: string;
+//   age: number;
   sayMyName(): string; // mesma assinatura da classe
 }
 
 class Person implements IPerson {
-  id: number;
-  name: string;
-  age: number;
+  readonly id: number;
+  protected name: string;
+  private age: number;
 
   constructor(id: number, name: string, age: number) {
     this.id = id;
@@ -21,6 +21,6 @@ class Person implements IPerson {
   }
 }
 
-const person = new Person(1, "Matheus", 5);
-
-console.log(person.sayMyName()); // Matheus
+// const person = new Person(1, "Matheus", 5);
+const felipe = new Person(1, "Matheus", 5);
+// console.log(person.sayMyName()); // Matheus
