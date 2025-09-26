e982ed9b14a6fb93ce4770d9cac7a216d430e6dc
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-26 15:52:45 -0300

    Classes Typescipt

---
+++ src/classes.ts
@@ -0,0 +1,26 @@
+interface IPerson {
+  id: number;
+  name: string;
+  age: number;
+  sayMyName(): string; // mesma assinatura da classe
+}
+
+class Person implements IPerson {
+  id: number;
+  name: string;
+  age: number;
+
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  sayMyName(): string {
+    return this.name;
+  }
+}
+
+const person = new Person(1, "Matheus", 5);
+
+console.log(person.sayMyName()); // Matheus
