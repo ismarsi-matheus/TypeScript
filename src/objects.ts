cfab8d928551d5b6f8aba45b8021e67fe5154e39
ismarsi-matheus <matheusleo.senac@gmail.com>
2025-09-26 13:09:45 -0300

    Propriedades possivelmente nulas

--- src/objects.ts
+++ src/objects.ts
@@ -20,3 +20,23 @@ const user: User = {
   password: "12345",
   orders: [{ productId: "1", price: 200 }],
 };
+
+user.password;
+
+const print = (message?: string) => {};
+
+print(user.password!);
+
+// Union
+
+type Author = {
+  books: string[];
+};
+
+const author: Author & User = {
+    age: 2,
+    books:['Livro 1'],
+    email:'jhon@doe.com',
+    name:'Jhon',
+    orders:[],
+};
